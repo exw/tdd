3c43c25de15521b904ebf8c8cd8b3f3f4b82cfb6
exw <exw@openmailbox.org>
2014-11-30 14:43:41 -0500

    Not sure why tests not running properly

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.PhantomJS()
-<<<<<<< HEAD
-browser.get = ('http://localhost:8000')
-=======
-browser.get('http://localhost:8000')
->>>>>>> 1a72bafcf4768af1487c8efd7b56d16ccf86610b
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        browser.get = ('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+    if __name__ == '__main__':
+        unittest.main(warnings='ignore')

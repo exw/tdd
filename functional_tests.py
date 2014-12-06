0573c8afa2146d3554b4e36398d0b97f43030e6b
exw <exw@openmailbox.org>
2014-12-06 12:05:39 -0500

    done through pg60, onto ORM

--- functional_tests.py
+++ functional_tests.py
@@ -7,10 +7,15 @@ import django.http
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.PhantomJS()
-        self.browser.implicitly_wait(3)
+        # self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Visit webpage
@@ -32,9 +37,16 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # After refresh page should update with item
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Test todo list can retain multiple items
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        # After refresh page should update with multiple items
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers')
 
         self.fail('Finish the test')
         

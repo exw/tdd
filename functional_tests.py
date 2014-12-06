6435a9054e3d48679f99c9a2dc256564cc38a979
exw <exw@openmailbox.org>
2014-12-06 14:07:55 -0500

    Strange error: Element does not exist in cache? Why?

--- functional_tests.py
+++ functional_tests.py
@@ -2,17 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-import django.http
+# import django.http
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.PhantomJS()
-        # self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
+    def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
